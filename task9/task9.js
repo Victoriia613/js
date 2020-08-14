2a029a591445b999bdd626b171c8ef0502b6333e
Victoriia613 <vikaozhozhenko@gmail.com>
2020-08-14 10:25:45 +0300

    almost done

--- task9/task9.js
+++ task9/task9.js
@@ -18,28 +18,137 @@
 
 
 /////////////////////////CALLBACKS//////////////////////////////
+function randomTimeout(){
+  return 1000-0.5+Math.random()* (5000-1000+1);
+}
 
 
-// function wakeUp(isAlarmOn, cb) {
-//     setTimeout(() => {
-// if(isAlarmOn) {
-//     // document.write(null, "I am up!")
-//     cb(null, "I am up!");
-// }
-// else {
-// // document.write("Noo, I overslept. I will be late!",null)
-// cb("Noo, I overslept. I will be late!", null)
-// }
-//     }, randomTimeout())
-// }
+function wakeUp(isAlarmOn, cb) {
+    setTimeout(() => {
+isAlarmOn 
+? cb(null, 'I am up!')
+: cb('Noo, I overslept');
+}, randomTimeout())
+}
 
-// wakeUp(true, (result) => {
-//     console.log(result)
-//     // console.log(negative)
+function takeAShower(isThereWater, cb){
+  setTimeout(() => {
+    isThereWater
+    ? cb(null, 'I am going to take a shower')
+    : cb('There is no hot water in the building')
+    
+  }, randomTimeout())
+}
 
-// })
+      function walkMyDog(isHeUpToIt, cb){
+    setTimeout(() => {
+      isHeUpToIt
+      ? cb(null,'I am taking a dog for a walk')
+      : cb('Dog is asleep')
+    },randomTimeout())  
+}
 
 
+function breakfast(doIWannaEat, cb){
+    setTimeout(() => {
+      doIWannaEat
+      ? cb(null,'I am eating breakfast')
+      : cb('There are no food in the fridge')
+      
+    }, randomTimeout()) 
+}
+
+function goToWork(workHard, cb){
+      setTimeout(() => {
+        workHard
+        ? cb(null, 'Starting to work now')
+        : cb('There is quaranteen at the office. I have to go home!')
+        
+      }, randomTimeout())
+  }
+  
+  function coffeeBreak(doIWannaCoffee, cb){
+      setTimeout(() => {
+        doIWannaCoffee
+        ? cb(null, 'I am going to get some coffee')
+        : cb('There are no coffee left')
+        
+      }, randomTimeout())
+    }
+
+    function dinnerWithFriends(isWorkDone, cb){
+    setTimeout(() => {
+      isWorkDone
+      ? cb(null, 'Going to have a dinner with friends now')
+      : cb('I have call with the customer this evening, I can not make it to the dinner')
+      
+    }, randomTimeout())
+}
+
+function goToSleep(amITired, cb){
+    setTimeout(() => {
+      amITired
+      ? cb(null, 'I am going to bad, goodnight')
+      : cb ('I do not want to sleep yet so I am gonna watch a movie!!!')
+      
+    }, randomTimeout())
+}
+
+
+wakeUp((error, data) => {
+if (error) {
+  console.log(error, 'in wakeUp')
+} else{
+  console.log(data)
+  takeAShower(true, (error, data) => {
+    if (error) {
+      console.log(error, 'in the shower')
+    } else{
+      console.log(data)
+      breakfast(true, (error, data) => {
+        if (error) {
+          console.log(error, 'at breakfast')
+        } else{
+          console.log(data)
+          walkMyDog(true, (error, data) => {
+            if (error) {
+              console.log(error, 'with dog')
+            } else{
+              console.log(data)
+              goToWork(true, (error, data) => {
+                if (error) {
+                  console.log(error, 'at work')
+                } else{
+                  console.log(data)
+                  coffeeBreak(true, (error, data) => {
+                    if (error) {
+                      console.log(error, 'at coffee break')
+                    } else{
+                      console.log(data)
+                      dinnerWithFriends(true, (error, data) => {
+                        if (error) {
+                          console.log(error, 'with dinner')
+                        } else{
+                          console.log(data)
+                          goToSleep(true, (error, data) => {
+                            if (error) {
+                              console.log(error, 'with sleep')
+                            } else{
+                              console.log(data)
+                            }})
+                        }})
+                    }})
+                }})
+              }})
+            }})
+          }})
+        }})
+ 
+
+
+    
+          
+        
 
 
 
@@ -71,114 +180,117 @@
 
 ///////////////////PROMISES///////////////////////////
 
-let chance = 0.1;
-function randomTimeout(){
-  return 1000-0.5+Math.random()* (5000-1000+1);
-}
-function wakeUp (isAlarmOn){
-  console.log('It is time to wake up')
-return new Promise ((resolve)=>{
-  setTimeout(() => {
-    Math.random() > chance
-    ?resolve(isAlarmOn)
-    :reject('I overslept')
+// let chance = 0.1;
+// function randomTimeout(){
+//   return 1000-0.5+Math.random()* (5000-1000+1);
+// }
+
+
+
+// function wakeUp (isAlarmOn){
+//   console.log('It is time to wake up')
+// return new Promise ((resolve)=>{
+//   setTimeout(() => {
+//     Math.random() > chance
+//     ?resolve(isAlarmOn)
+//     :reject('I overslept')
     
-  }, randomTimeout());
-})
+//   }, randomTimeout());
+// })
 
-}
-function takeAShower(isThereWater){
-  console.log('I am going to take a shower')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(isThereWater)
-      :reject('There is no hot water in the building')
+// }
+// function takeAShower(isThereWater){
+//   console.log('I am going to take a shower')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(isThereWater)
+//       :reject('There is no hot water in the building')
       
-    }, randomTimeout())
-  })
+//     }, randomTimeout())
+//   })
   
-}
+// }
 
-function walkMyDog(isHeUpToIt){
-  console.log('I am taking a dog for a walk')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(isHeUpToIt)
-      :reject('Dog is asleep')
+// function walkMyDog(isHeUpToIt){
+//   console.log('I am taking a dog for a walk')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(isHeUpToIt)
+//       :reject('Dog is asleep')
       
-    },randomTimeout())
-  })
+//     },randomTimeout())
+//   })
   
-}
+// }
 
 
-function breakfast(doIWannaEat){
-  console.log('I am eating breakfast')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(doIWannaEat)
-      :reject('There are no food in the fridge')
+// function breakfast(doIWannaEat){
+//   console.log('I am eating breakfast')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(doIWannaEat)
+//       :reject('There are no food in the fridge')
       
-    }, 2000)
-  })
+//     }, 2000)
+//   })
   
-}
+// }
 
 
-function goToWork(workHard){
-  console.log('Starting to work now')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(workHard)
-      :reject('There is quaranteen at the office. I have to go home!')
+// function goToWork(workHard){
+//   console.log('Starting to work now')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(workHard)
+//       :reject('There is quaranteen at the office. I have to go home!')
       
-    }, 2000)
-  })
+//     }, 2000)
+//   })
   
-}
+// }
 
-function coffeeBreak(doIWannaCoffee){
-  console.log('I am going to get some coffee')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(doIWannaCoffee)
-      :reject('There are no coffee left')
+// function coffeeBreak(doIWannaCoffee){
+//   console.log('I am going to get some coffee')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(doIWannaCoffee)
+//       :reject('There are no coffee left')
       
-    }, 2000)
-  })
+//     }, 2000)
+//   })
   
-}
+// }
 
-function dinnerWithFriends(isWorkDone){
-  console.log('Going to have a dinner with friends now')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(isWorkDone)
-      :reject('I have call with the customer this evening, I can not make it to the dinner')
+// function dinnerWithFriends(isWorkDone){
+//   console.log('Going to have a dinner with friends now')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(isWorkDone)
+//       :reject('I have call with the customer this evening, I can not make it to the dinner')
       
-    }, 2000)
-  })
+//     }, 2000)
+//   })
   
-}
+// }
 
-function goToSleep(amITired){
-  console.log('I am going to bad, goodnight')
-  return new Promise ((resolve,reject)=>{
-    setTimeout(() => {
-      Math.random() > chance
-      ?resolve(amITired)
-      :reject('I do not want to sleep yet so I am gonna watch a movie!!!')
+// function goToSleep(amITired){
+//   console.log('I am going to bad, goodnight')
+//   return new Promise ((resolve,reject)=>{
+//     setTimeout(() => {
+//       Math.random() > chance
+//       ?resolve(amITired)
+//       :reject('I do not want to sleep yet so I am gonna watch a movie!!!')
       
-    }, 2000)
-  })
+//     }, 2000)
+//   })
   
-}
+// }
 
 // wakeUp('I am up!') 
 // .then((result) => {
@@ -208,19 +320,19 @@ function goToSleep(amITired){
 // })
 
 ////////////////////////////////////////ASYNC AWAIT///////////////////////////////////////////
-async function myDay() {
-  try {
-   let start = await wakeUp();
-    let shower = await takeAShower();
-    let walk = await walkMyDog();
-    let eat = await breakfast();
-    let work = await goToWork();
-    let coffee = await coffeeBreak();
-    let dinner = await dinnerWithFriends();
-    let sleep = await goToSleep();
+// async function myDay() {
+//   try {
+//    let start = await wakeUp();
+//     let shower = await takeAShower();
+//     let walk = await walkMyDog();
+//     let eat = await breakfast();
+//     let work = await goToWork();
+//     let coffee = await coffeeBreak();
+//     let dinner = await dinnerWithFriends();
+//     let sleep = await goToSleep();
 
-  } catch (e) {
-    console.error(e);
-  }
-}
-myDay();
+//   } catch (e) {
+//     console.error(e);
+//   }
+// }
+// myDay();
